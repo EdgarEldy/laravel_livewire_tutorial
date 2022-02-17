314629de01c5d5612433dd6108ab68308c9d7eaa
Edgar Eldy <ed.eldy21@live.com>
2022-02-17 21:20:25 +0200

    Set string length to 100

--- database/migrations/2022_02_17_185908_create_products_table.php
+++ database/migrations/2022_02_17_185908_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->foreignId('category_id')->constrained('categories');
-            $table->string('product_name');
+            $table->string('product_name', 100);
             $table->float('unit_price');
             $table->timestamps();
         });
