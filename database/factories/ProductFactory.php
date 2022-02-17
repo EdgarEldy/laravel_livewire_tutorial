0af3aa162eeff5cf3b0ad2a3de21d1a30490fad9
Edgar Eldy <ed.eldy21@live.com>
2022-02-17 21:04:43 +0200

    Add fake products data for testing

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
 
 class ProductFactory extends Factory
 {
@@ -16,8 +17,12 @@ class ProductFactory extends Factory
     {
         // Load categories
         $categories = array_column(Category::all()->toArray(), 'id');
+
         return [
-            //
+            // Add fake products data for testing
+            'category_id' => Arr::random($categories),
+            'product_name' => $this->faker->name,
+            'unit_price' => $this->faker->randomNumber(4)
         ];
     }
 }
