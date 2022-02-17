5c41f6c8ced3c44e3bc34a2405ac834e6941fb94
Edgar Eldy <ed.eldy21@live.com>
2022-02-17 21:33:19 +0200

    Add orders data for testing

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -2,7 +2,10 @@
 
 namespace Database\Factories;
 
+use App\Models\Customer;
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
 
 class OrderFactory extends Factory
 {
@@ -13,8 +16,15 @@ class OrderFactory extends Factory
      */
     public function definition()
     {
+        $customers= array_column(Customer::all()->toArray(), 'id');
+        $products= array_column(Product::all()->toArray(), 'id');
+
         return [
-            //
+            // Add orders data for testing
+            'customer_id' => Arr::random($customers),
+            'product_id' => Arr::random($products),
+            'qty' => $this->faker->randomNumber(2),
+            'total' => $this->faker->randomNumber(4)
         ];
     }
 }
