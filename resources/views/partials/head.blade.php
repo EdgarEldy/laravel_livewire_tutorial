955b189c58ab8e1a041613d4257f451b61dde002
Edgar Eldy <ed.eldy21@live.com>
2022-02-21 22:26:45 +0200

    Add head partial

---
+++ resources/views/partials/head.blade.php
@@ -0,0 +1,13 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>LARAVEL LIVEWIRE TUTORIAL</title>
+
+    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('css/datepicker3.css')}}" rel="stylesheet">
+    <link href="{{asset('css/bootstrap-table.css')}}" rel="stylesheet">
+    <link href="{{asset('css/styles.css')}}" rel="stylesheet">
+    <link href="{{asset('css/fontawesome.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('css/chosen.min.css')}}" rel="stylesheet">
+
+</head>
