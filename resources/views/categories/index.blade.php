d1d65dd174c9059fbb7f7e6b05da829d89cb51c0
Edgar Eldy <ed.eldy21@live.com>
2022-02-23 17:04:36 +0200

    Include categories.data view component

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.default')
+@section('content')
+    <div class="col-lg-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">Product categories</div>
+            <div class="panel-body">
+                <a href="{{url('categories/create')}}"
+                   class="btn btn-primary">New</a>
+                <!-- Categories data -->
+                <livewire:categories.data/>
+            </div>
+        </div>
+    </div>
+
+@endsection()
