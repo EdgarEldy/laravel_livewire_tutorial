1991b508b9b47e9970141efed50b3be4cb4e1248
Edgar Eldy <ed.eldy21@live.com>
2022-02-23 17:03:51 +0200

    Retreive categories from database

--- app/Http/Livewire/Categories/Data.php
+++ app/Http/Livewire/Categories/Data.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Livewire\Categories;
 
+use App\Models\Category;
 use Livewire\Component;
 
 class Data extends Component
 {
     public function render()
     {
-        return view('livewire.categories.data');
+        $categories = Category::all();
+        return view('livewire.categories.data',[
+            'categories' => $categories
+        ]);
     }
 }
