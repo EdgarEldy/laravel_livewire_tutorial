e3a9fff8c5f54cf8dc7ddb4e319f11a67bd49cac
Edgar Eldy <ed.eldy21@live.com>
2022-03-01 22:00:49 +0200

    Define rules

--- app/Http/Livewire/Categories/Form.php
+++ app/Http/Livewire/Categories/Form.php
@@ -9,6 +9,16 @@ class Form extends Component
     // Public properties
     public $category_id, $category_name;
 
+    // Define rules
+    protected $rules;
+
+    public function hydrate()
+    {
+        $this->rules = [
+            'category_name' => ['required'],
+        ];
+    }
+
     public function render()
     {
         return view('livewire.categories.form');
