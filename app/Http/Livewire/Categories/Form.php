2f53c8b4c2339980451e8959b35e7c5a44c09e37
Edgar Eldy <ed.eldy21@live.com>
2022-03-01 21:55:07 +0200

    Add categories properties

--- app/Http/Livewire/Categories/Form.php
+++ app/Http/Livewire/Categories/Form.php
@@ -6,7 +6,9 @@ use Livewire\Component;
 
 class Form extends Component
 {
-    
+    // Public properties
+    public $category_id, $category_name;
+
     public function render()
     {
         return view('livewire.categories.form');
