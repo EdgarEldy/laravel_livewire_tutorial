2d9668cee5b7b0edc8fcff29f8cf8651760ef7db
Edgar Eldy <ed.eldy21@live.com>
2022-03-31 22:26:59 +0300

    Add submit function for creating or updating a product

--- app/Http/Livewire/Products/Form.php
+++ app/Http/Livewire/Products/Form.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire\Products;
 
 use App\Models\Category;
+use App\Models\Product;
 use Livewire\Component;
 
 class Form extends Component
@@ -33,6 +34,34 @@ class Form extends Component
     public function closeModal()
     {
         $this->reset();
+    }
+
+    // Add submit function for creating or updating a product
+    public function submit()
+    {
+        // Validate inputs
+        $this->validate();
+
+        $product = $this->product_id ? Product::find($this->product_id) : new Product();
+
+        $product->category_id = $this->category_id;
+        $product->product_name = $this->product_name;
+        $product->unit_price = $this->unit_price;
+        $product->save();
+
+        // Reset inputs
+        $this->reset();
+
+        $this->dispatchBrowserEvent('close-modal', [
+            'modalname' => "modalFormProduct"
+        ]);
+
+        $this->dispatchBrowserEvent('notify-success', [
+            'message' => "Product has been saved successfully !"
+        ]);
+
+        $this->emit('productsList');
+
     }
 
     public function render()
