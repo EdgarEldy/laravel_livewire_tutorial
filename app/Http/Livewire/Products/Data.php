7591f57b204007205a5b8afcb0fa14ea031ee118
Edgar Eldy <ed.eldy21@live.com>
2022-03-31 11:28:34 +0200

    Show products with categories and paginate per 10

--- app/Http/Livewire/Products/Data.php
+++ app/Http/Livewire/Products/Data.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Livewire\Products;
 
+use App\Models\Product;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Data extends Component
 {
+    use WithPagination;
+    
     // Add productsList listener
     public $listeners = ['productsList'];
+
+    // Show products with categories
+    public function productsList()
+    {
+        $products = Product::with('category')->paginate(10);
+        return $products;
+    }
 
     public function render()
     {
