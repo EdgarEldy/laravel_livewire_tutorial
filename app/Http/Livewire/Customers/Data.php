706b624030a498440a2e4c2d61cb7206ed865a60
Edgar Eldy <ed.eldy21@live.com>
2022-04-03 21:44:08 +0300

    Setting up pagination trait

--- app/Http/Livewire/Customers/Data.php
+++ app/Http/Livewire/Customers/Data.php
@@ -3,9 +3,13 @@
 namespace App\Http\Livewire\Customers;
 
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Data extends Component
 {
+    // Setting up pagination trait
+    use WithPagination;
+
     public function render()
     {
         return view('livewire.customers.data');
