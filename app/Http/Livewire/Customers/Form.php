f281ab6c234e42b4c7634e587ba20567e7c8b9ef
Edgar Eldy <ed.eldy21@live.com>
2022-04-03 22:10:03 +0300

    Add customers public properties

--- app/Http/Livewire/Customers/Form.php
+++ app/Http/Livewire/Customers/Form.php
@@ -6,6 +6,14 @@ use Livewire\Component;
 
 class Form extends Component
 {
+    // Add customers public properties
+    public $customer_id;
+    public $first_name;
+    public $last_name;
+    public $tel;
+    public $email;
+    public $address;
+
     public function render()
     {
         return view('livewire.customers.form');
