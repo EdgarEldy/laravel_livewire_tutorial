41423d4762ff55dbf5c8f7cc657b79b8c00d9dbb
Edgar Eldy <ed.eldy21@live.com>
2022-04-03 21:57:56 +0300

    Add customers/index route

--- routes/customers.php
+++ routes/customers.php
@@ -1,3 +1,6 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/customers', [CustomerController::class, 'index'])->name('customers.index');
