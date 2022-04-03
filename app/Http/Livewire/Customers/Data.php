985c7c71f963d8bdcd05e221a2af6e1e337c7c02
Edgar Eldy <ed.eldy21@live.com>
2022-04-03 21:52:33 +0300

    Get customers and paginate per 10

--- app/Http/Livewire/Customers/Data.php
+++ app/Http/Livewire/Customers/Data.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire\Customers;
 
+use App\Models\Customer;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -9,9 +10,16 @@ class Data extends Component
 {
     // Setting up pagination trait
     use WithPagination;
-    
+
     // Add customersList event listener
     protected $listeners= ['customersList'];
+
+    // Get customers and paginate per 10
+    public function customersList()
+    {
+        $customers = Customer::paginate(10);
+        return $customers;
+    }
 
     public function render()
     {
