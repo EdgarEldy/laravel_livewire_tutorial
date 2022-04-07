524b616e9499065abd4c65c285034aac52e11604
Edgar Eldy <ed.eldy21@live.com>
2022-04-07 08:33:16 +0300

    Show orders with customers and products and paginate per 10 orders

--- app/Http/Livewire/Orders/Data.php
+++ app/Http/Livewire/Orders/Data.php
@@ -2,13 +2,23 @@
 
 namespace App\Http\Livewire\Orders;
 
+use App\Models\Order;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Data extends Component
 {
 
+    use WithPagination;
     // Add ordersList listener
     public $listeners = ['ordersList'];
+
+    // Show orders with customers and products
+    public function ordersList()
+    {
+        $orders = Order::with(['customer', 'product'])->paginate(10);
+        return $orders;
+    }
 
     public function render()
     {
