3425b3529f9442ff96e1ad65f2e8c8a203146e66
Edgar Eldy <ed.eldy21@live.com>
2022-04-07 08:29:45 +0300

    Add ordersList listener

--- app/Http/Livewire/Orders/Data.php
+++ app/Http/Livewire/Orders/Data.php
@@ -6,6 +6,10 @@ use Livewire\Component;
 
 class Data extends Component
 {
+
+    // Add ordersList listener
+    public $listeners = ['ordersList'];
+
     public function render()
     {
         return view('livewire.orders.data');
