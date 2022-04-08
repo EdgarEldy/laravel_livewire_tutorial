decba91a1103668ad4259aee540faaac1e30c7cc
Edgar Eldy <ed.eldy21@live.com>
2022-04-08 11:32:42 +0200

    Add orders public properties

--- app/Http/Livewire/Orders/Form.php
+++ app/Http/Livewire/Orders/Form.php
@@ -6,6 +6,15 @@ use Livewire\Component;
 
 class Form extends Component
 {
+    // Add orders public properties
+    public $order_id;
+    public $customer_id;
+    public $category;
+    public $product_id;
+    public $product;
+    public $qty;
+    public $total;
+
     public function render()
     {
         return view('livewire.orders.form');
