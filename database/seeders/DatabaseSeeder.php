81330913bbfe3316894802e7fe113f0116e840ea
Edgar Eldy <ed.eldy21@live.com>
2022-06-08 20:54:36 +0200

    Register RoleSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,8 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class,
             ProductSeeder::class,
             CustomerSeeder::class,
-            OrderSeeder::class
+            OrderSeeder::class,
+            RoleSeeder::class
         ]);
     }
 }
