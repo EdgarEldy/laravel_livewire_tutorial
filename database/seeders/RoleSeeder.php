0e6d039f25a4375283046704c486cf8aaec54c50
Edgar Eldy <ed.eldy21@live.com>
2022-06-08 20:14:52 +0200

    Add Admin and User role seeders

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use Illuminate\Database\Seeder;
 
 class RoleSeeder extends Seeder
@@ -13,6 +14,16 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Role::upsert([
+            [
+                'role_name' => 'Admin'
+            ],
+            [
+                'role_name' => 'User'
+            ]
+        ],
+            [
+                'role_name'
+            ]);
     }
 }
