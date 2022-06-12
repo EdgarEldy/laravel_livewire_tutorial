27f4750d501b96d5bf6fd0d365d91c50e557bd03
Edgar Eldy <ed.eldy21@live.com>
2022-06-12 21:56:02 +0200

    Register UserSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
             OrderSeeder::class,
             RoleSeeder::class,
             PermissionSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
