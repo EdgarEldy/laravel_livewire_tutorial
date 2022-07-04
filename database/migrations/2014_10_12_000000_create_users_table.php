bae49e255253c197093821e2255a5cc87acc7f68
Edgar Eldy <ed.eldy21@live.com>
2022-07-04 23:14:33 +0200

    Revert "Update create_users_table migration : Add first_name,last_name,tel and address columns"
    This reverts commit 786016e4317308b069c46f3135e87e1b60c16f02.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('tel');
+            $table->string('name');
             $table->string('email')->unique();
-            $table->string('address');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
