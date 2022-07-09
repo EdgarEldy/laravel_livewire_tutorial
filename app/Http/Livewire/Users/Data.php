1a34a0481962944776e7a34e749d90942c1ab0d1
Edgar Eldy <ed.eldy21@live.com>
2022-07-09 23:25:43 +0200

    Setting up paginations

--- app/Http/Livewire/Users/Data.php
+++ app/Http/Livewire/Users/Data.php
@@ -3,9 +3,13 @@
 namespace App\Http\Livewire\Users;
 
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Data extends Component
 {
+    // Setting up paginations
+    use WithPagination;
+
     public function render()
     {
         return view('livewire.users.data');
