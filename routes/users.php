046a797da613a71e7960b9b601c6344f8c939a79
Edgar Eldy <ed.eldy21@live.com>
2022-07-16 23:08:13 +0200

    Register users/create route

--- routes/users.php
+++ routes/users.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
