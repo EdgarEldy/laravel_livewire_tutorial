1bd5cfdffcada890ac673ebaa302042ea5ce304c
EdgarEldy <ed.eldy21@live.com>
2022-07-17 21:01:44 +0200

    Add rolesList event listener

--- app/Http/Livewire/Roles/Data.php
+++ app/Http/Livewire/Roles/Data.php
@@ -9,6 +9,10 @@ class Data extends Component
 {
     // Setting up paginations
     use WithPagination;
+
+    // Add rolesList event listener
+    protected $listeners = ['rolesList'];
+
     public function render()
     {
         return view('livewire.roles.data');
