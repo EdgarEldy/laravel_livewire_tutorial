9896e2f69a188c4bda031ac3c9435f2212516cd4
Edgar Eldy <ed.eldy21@live.com>
2022-07-17 22:08:48 +0200

    Add Role's public properties

--- app/Http/Livewire/Roles/Form.php
+++ app/Http/Livewire/Roles/Form.php
@@ -6,6 +6,10 @@ use Livewire\Component;
 
 class Form extends Component
 {
+    // Public properties
+    public $role_id;
+    public $role_name;
+
     public function render()
     {
         return view('livewire.roles.form');
