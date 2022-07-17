168b3f260a6cb4abf221239700aa683994dba64e
EdgarEldy <ed.eldy21@live.com>
2022-07-17 21:00:38 +0200

    Setting up paginations

--- app/Http/Livewire/Roles/Data.php
+++ app/Http/Livewire/Roles/Data.php
@@ -3,9 +3,12 @@
 namespace App\Http\Livewire\Roles;
 
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Data extends Component
 {
+    // Setting up paginations
+    use WithPagination;
     public function render()
     {
         return view('livewire.roles.data');
