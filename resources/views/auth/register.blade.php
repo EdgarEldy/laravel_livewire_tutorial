e721d6496de7ee4c75292737c4dcd512729b519a
Edgar Eldy <ed.eldy21@live.com>
2022-07-24 22:04:08 +0200

    Redesing auth/register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,60 +1,61 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+@extends('layouts.default')
+@section('content')
+    <div class="col-lg-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">Add a user</div>
+            <div class="panel-body">
+                <div class="col-md-6">
 
-        <x-jet-validation-errors class="mb-4" />
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
-
-            <div>
-                <x-jet-label for="name" value="{{ __('Name') }}" />
-                <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
-
-            @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
-                <div class="mt-4">
-                    <x-jet-label for="terms">
-                        <div class="flex items-center">
-                            <x-jet-checkbox name="terms" id="terms"/>
-
-                            <div class="ml-2">
-                                {!! __('I agree to the :terms_of_service and :privacy_policy', [
-                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Terms of Service').'</a>',
-                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Privacy Policy').'</a>',
-                                ]) !!}
-                            </div>
+                        <div class="form-group">
+                            <label for="first_name">First Name</label>
+                            <input id="first_name" class="form-control" type="text" name="first_name"
+                                   :value="old('first_name')" required autofocus autocomplete="first_name"/>
                         </div>
-                    </x-jet-label>
+
+                        <div class="form-group">
+                            <label for="last_name">Last Name</label>
+                            <input id="last_name" class="form-control" type="text" name="last_name"
+                                   :value="old('last_name')" required autofocus autocomplete="last_name"/>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="tel">Telephone</label>
+                            <input id="tel" class="form-control" type="text" name="tel"
+                                   :value="old('tel')" required autofocus autocomplete="tel"/>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input id="email" class="form-control" type="email" name="email"
+                                   :value="old('email')" required/>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="address">Address</label>
+                            <input id="address" class="form-control" type="text" name="address"
+                                   :value="old('address')" required/>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input id="password" class="form-control" type="password" name="password"
+                                   required autocomplete="new-password"/>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password_confirmation">Confirm Password</label>
+                            <input id="password_confirmation" class="form-control" type="password"
+                                   name="password_confirmation" required autocomplete="new-password"/>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Save</button>
+                        <button type="reset" class="btn btn-default">Reset</button>
+
+                    </form>
                 </div>
-            @endif
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-jet-button class="ml-4">
-                    {{ __('Register') }}
-                </x-jet-button>
             </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+        </div>
+    </div><!-- /.col-->
+@endsection()
