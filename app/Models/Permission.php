3a6da75c27efe958c9c32c6c077d6772156bc0a1
EdgarEldy <ed.eldy21@live.com>
2022-08-30 16:03:44 +0200

    Add permission_name as a fillable attribute

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Permission extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['permission_name'];
 }
