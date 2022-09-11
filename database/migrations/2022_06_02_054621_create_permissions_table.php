a813b585b54929300272c7a110ba2f9bb0b0d827
Edgar Eldy <ed.eldy21@live.com>
2022-09-11 12:09:30 +0200

    Remove is_global column

--- database/migrations/2022_06_02_054621_create_permissions_table.php
+++ database/migrations/2022_06_02_054621_create_permissions_table.php
@@ -16,7 +16,6 @@ class CreatePermissionsTable extends Migration
         Schema::create('permissions', function (Blueprint $table) {
             $table->id();
             $table->string('name', 100);
-            $table->boolean('is_global')->default(false);
             $table->timestamps();
         });
     }
