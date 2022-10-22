0a9c892263e5164947f80d5567c714e5af5f7d29
EdgarEldy <ed.eldy21@live.com>
2022-10-22 21:14:34 +0200

    Add users public properties

--- app/Http/Livewire/Users/Form.php
+++ app/Http/Livewire/Users/Form.php
@@ -6,6 +6,16 @@ use Livewire\Component;
 
 class Form extends Component
 {
+    // Add users public properties
+    public $user_id;
+    public $first_name;
+    public $last_name;
+    public $tel;
+    public $email;
+    public $address;
+    public $password;
+    public $confirm_password;
+
     public function render()
     {
         return view('livewire.users.form');
