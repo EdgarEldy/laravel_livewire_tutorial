dbce1929b60fe28b57b4491830cb0bc2bb978a2b
EdgarEldy <ed.eldy21@live.com>
2023-06-22 19:36:14 +0200

    Add parent_id column

--- database/migrations/2022_06_02_054621_create_permissions_table.php
+++ database/migrations/2022_06_02_054621_create_permissions_table.php
@@ -15,6 +15,7 @@ class CreatePermissionsTable extends Migration
     {
         Schema::create('permissions', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('parent_id')->nullable()->constrained('permissions')->cascadeOnDelete();
             $table->string('permission_name', 100);
             $table->timestamps();
         });
