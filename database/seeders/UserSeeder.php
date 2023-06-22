d6dac33869625dcba569aa517beabca1d0851c82
EdgarEldy <ed.eldy21@live.com>
2023-06-22 19:38:35 +0200

    Get Admin role

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -48,5 +49,8 @@ class UserSeeder extends Seeder
         User::factory()
             ->count(10)
             ->create();
+
+        // Get Admin role
+        $admin_role = Role::whereRoleName('Admin')->first();
     }
 }
