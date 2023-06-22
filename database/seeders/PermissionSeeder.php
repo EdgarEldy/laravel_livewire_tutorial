86fdc3e388616cbac29b9de2c86b75cce03b7f99
EdgarEldy <ed.eldy21@live.com>
2023-06-22 19:26:32 +0200

    Update permissions based on model names

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -20,47 +20,13 @@ class PermissionSeeder extends Seeder
     {
         // Creating permissions array
         $permissions = [
-            // categories
-            'Create a category',
-            'Show categories',
-            'Show category details',
-            'Update a category',
-            'Remove a category',
-
-            // Products
-            'Create a product',
-            'Show products',
-            'Show product details',
-            'Update a product',
-            'Remove a product',
-
-            // Customers
-            'Create a customer',
-            'Show customers',
-            'Show customer details',
-            'Update a customer',
-            'Remove a customer',
-
-            // Orders
-            'Create an order',
-            'Show orders',
-            'Shown order details',
-            'Update an order',
-            'Remove an order',
-
-            // Roles
-            'Create a role',
-            'Show roles',
-            'Show role details',
-            'Update a role',
-            'Remove a role',
-
-            // Users
-            'Create a user',
-            'Show users',
-            'Show user details',
-            'Update a user',
-            'Remove a user',
+            'Category',
+            'Product',
+            'Customer',
+            'Order',
+            'Role',
+            'User',
+            'Permission',
         ];
 
         // Get admin role
